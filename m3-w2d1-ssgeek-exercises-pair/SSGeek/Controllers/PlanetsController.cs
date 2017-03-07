ce355547d9a32060f44bc86a0682496ca4a65f3b
Andrew Jaros <amjaros1@gmail.com>
2017-03-07 14:52:05 -0500

    completed pair tues

--- m3-w2d1-ssgeek-exercises-pair/SSGeek/Controllers/PlanetsController.cs
+++ m3-w2d1-ssgeek-exercises-pair/SSGeek/Controllers/PlanetsController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SSGeek.DAL;
+using SSGeek.Models;
 
 namespace SSGeek.Controllers
 {
@@ -12,6 +14,50 @@ namespace SSGeek.Controllers
         public ActionResult Index()
         {
             return View("Index");
+        }
+
+        public ActionResult ForumPost()
+        {
+            IForumPostDAL dal = new ForumPostSqlDAL();
+
+            List<ForumPost> results = dal.GetAllPosts();
+
+            return View(results);
+        }
+
+        public ActionResult PostAMessage()
+        {
+            if(Request.Params["Username"] != null)
+            {
+            IForumPostDAL dal = new ForumPostSqlDAL();
+            ForumPost post = new ForumPost();
+
+            post.Username = Request.Params["Username"];
+            post.Subject = Request.Params["Subject"];
+            post.Message = Request.Params["Message"];
+
+            bool result = dal.SaveNewPost(post);
+
+            ViewBag.Result = result;
+            }
+
+            return View();
+        }
+
+        public ActionMethod CreateUser()
+        {
+            // Do some stuff
+            return View();
+        }
+        [HttpPost]
+        public ActionMethod CreateUser(ViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Save to db
+                return RedirectToAction("CreateUser");
+            }
+            return View(model);
         }
     }
 }
--- m3-w2d1-ssgeek-exercises-pair/SSGeek/DAL/ForumPostSqlDAL.cs
+++ m3-w2d1-ssgeek-exercises-pair/SSGeek/DAL/ForumPostSqlDAL.cs
@@ -8,16 +8,71 @@ using System.Data.SqlClient;
 namespace SSGeek.DAL
 {
     public class ForumPostSqlDAL : IForumPostDAL
-    {       
-        
+    {
+        string connectionString = @"Data Source=DESKTOP-6JSSBN8\SQLEXPRESS;Initial Catalog=ForumPost;Integrated Security=True;";
+        string SQL_GetAllPosts = "SELECT id, username, subject, message, post_date FROM forum_post;";
+        string SQL_SaveNewPost = "INSERT INTO forum_post (username, subject, message, post_date) VALUES (@username, @subject, @message, getdate());";
         public List<ForumPost> GetAllPosts()
         {
-			throw new NotImplementedException();
+            List<ForumPost> result = new List<ForumPost>();
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    SqlCommand cmd = new SqlCommand(SQL_GetAllPosts, conn);
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        ForumPost f = new ForumPost();
+                        f.Id = Convert.ToInt32(reader["id"]);
+                        f.Username = Convert.ToString(reader["username"]);
+                        f.Subject = Convert.ToString(reader["subject"]);
+                        f.Message = Convert.ToString(reader["message"]);
+                        f.PostDate = Convert.ToDateTime(reader["post_date"]);
+
+                        result.Add(f);
+                    }
+                }
+            }
+            catch (SqlException ex)
+            {
+                //Log and throw the exception
+                throw new NotImplementedException();
+            }
+
+            return result;
+
         }
 
         public bool SaveNewPost(ForumPost post)
         {
-            throw new NotImplementedException();
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    SqlCommand cmd = new SqlCommand(SQL_SaveNewPost, conn);
+
+                    cmd.Parameters.AddWithValue("@username", post.Username);
+                    cmd.Parameters.AddWithValue("@subject", post.Subject);
+                    cmd.Parameters.AddWithValue("@message", post.Message);
+
+                    int rowsAffected = cmd.ExecuteNonQuery();
+
+                    return (rowsAffected > 0) ? true : false;
+                }
+            }
+            catch (SqlException ex)
+            {
+                //Log and throw the exception
+                throw new NotImplementedException();
+            }
         }
     }
 }
